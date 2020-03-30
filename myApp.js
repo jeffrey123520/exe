2f97f4a053ae6e5e9a0a73253e6782a8ca432c7d
Glitch (tinted-faint-sweatpants) <none>
2020-03-30 12:33:09 +0000

    📯👩‍❤️‍💋‍👩 Checkpoint ./myApp.js:13525936/4444

--- myApp.js
+++ myApp.js
@@ -2,19 +2,16 @@ const User = require("./models/user.model");
 const Exercise = require("./models/exercise.model");
 
 module.exports = app => {
-  app.route("/api/exercise/new-user").post((req, res) => {
-    let x = "kkk";
-    User.findOne({ username: req.body.username }).then(() => {
-      x = "Found";
-    });
-    if (x == "kkk") {
-      const username = req.body.username;
-      const newUser = new User({ username });
-      newUser
-        .save()
-        .then(() => (x = "addded"))
-        .catch(err => res.status(400).json("Error"));
-    }
-    res.send("" + x);
+  app.route("/api/exercise/new-user/").post((req, res) => {
+    const username = req.body.username;
+    const newUser = new User({ username });
+    let x = "";
+    newUser
+      .save()
+      .then(() => {
+        console.log("Ritik Dua");
+      })
+      .catch(err => res.status(400).json("Error: " + err));
+    res.send(x);
   });
 };
